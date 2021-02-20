e46a4b415fb0e572758a4312ef91f2bbb7b3ab47
mochisvelas <velasquezmochis@gmail.com>
2021-02-20 01:13:51 -0600

    create models for application

---
+++ Models/NotificationModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailNotify.Models
+{
+    public class NotificationModel
+    {
+        public int Id { get; set; }
+
+        public ReceiverModel Receiver { get; set; }
+
+        public string Subject { get; set; }
+
+        public NotificationBodyModel Body { get; set; }
+    }
+}
