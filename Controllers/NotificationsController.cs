52447f6700a683052c1e390b8e8d6f5a83e950df
mochisvelas <velasquezmochis@gmail.com>
2021-02-22 16:09:55 -0600

    pending attaching image

--- Controllers/NotificationsController.cs
+++ Controllers/NotificationsController.cs
@@ -8,16 +8,21 @@ using EmailNotify.Data;
 using EmailNotify.Models;
 using SendGrid;
 using SendGrid.Helpers.Mail;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace EmailNotify.Controllers
 {
     public class NotificationsController : Controller
     {
         private readonly ReceiverContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public NotificationsController(ReceiverContext context)
+        public NotificationsController(ReceiverContext context, IWebHostEnvironment hostEnvironment)
         {
             _context = context;
+            _hostEnvironment = hostEnvironment;
         }
 
         public async Task<IActionResult> Index()
@@ -46,10 +51,20 @@ namespace EmailNotify.Controllers
         //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Notify([Bind("Receiver, Subject, Text, Image, Video, Link")] Notification notification)
+        public async Task<IActionResult> Notify([Bind("Receiver, Subject, Text, Image, Video, Link")] Notification notification)
         {
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                string fileName = Path.GetFileNameWithoutExtension(notification.Image);
+                string extension = Path.GetExtension(notification.Image);
+                notification.Image = fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                string path = Path.Combine(wwwRootPath + "/Image/", fileName);
+                //using (var fileStream = new FileStream(path, FileMode.Create))
+                //{
+                //    await notification.Image(fileStream);
+                //}
+
                 SendEmail(notification.Receiver, notification.Subject, notification.Text).Wait();
                 return RedirectToAction(nameof(Index));
             }
@@ -59,6 +74,7 @@ namespace EmailNotify.Controllers
         static async Task SendEmail(string Receiver, string Subject, string Text)
         {
             var apiKey = "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";
+            //var apiKey = Environment.GetEnvironmentVariable("SENGRID_API_KEY");
             var client = new SendGridClient(apiKey);
             var from = new EmailAddress("velasquezmochis@hotmail.com", "Brenner");
             var to = new EmailAddress(Receiver);
