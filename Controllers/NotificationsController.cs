a87f879fb690ae2cfa007f0974abaf2ff0265a76
mochisvelas <velasquezmochis@gmail.com>
2021-02-22 13:07:00 -0600

    email via sendgrid

--- Controllers/NotificationsController.cs
+++ Controllers/NotificationsController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using EmailNotify.Data;
 using EmailNotify.Models;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace EmailNotify.Controllers
 {
@@ -23,6 +25,7 @@ namespace EmailNotify.Controllers
             return View(await _context.Receiver.ToListAsync());
         }
 
+        //GET
         public async Task<IActionResult> Notify(int? id)
         {
             if(id == null)
@@ -31,11 +34,45 @@ namespace EmailNotify.Controllers
             }
 
             var receiver = await _context.Receiver.FindAsync(id);
+            
             if (receiver == null)
             {
                 return NotFound();
             }
-            return View(receiver);
+            ViewData["Email"] = receiver.Email;
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Notify([Bind("Receiver, Subject, Text, Image, Video, Link")] Notification notification)
+        {
+            if (ModelState.IsValid)
+            {
+                SendEmail(notification.Receiver, notification.Subject, notification.Text).Wait();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(notification);
+        }
+
+        static async Task SendEmail(string Receiver, string Subject, string Text)
+        {
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("velasquezmochis@hotmail.com", "Brenner");
+            var to = new EmailAddress(Receiver);
+            var subject = Subject;
+            var text = Text;
+            var html = "";
+            var message = MailHelper.CreateSingleEmail(
+                from,
+                to,
+                subject,
+                text,
+                html);
+
+            var response = await client.SendEmailAsync(message);
         }
     }
 }
--- Models/Notification.cs
+++ Models/Notification.cs
@@ -8,15 +8,20 @@ namespace EmailNotify.Models
 {
     public class Notification
     {
-        public int Id { get; set; }
 
-        public Receiver Receiver { get; set; }
+        public string Receiver { get; set; }
 
         public string Subject { get; set; }
 
-        public NotificationBody Body { get; set; }
+        public string Text { get; set; }
 
-        [DataType(DataType.Date)]
-        public DateTime SentDate { get; set; }
+        public string Image { get; set; }
+
+        public string Video { get; set; }
+
+        public string Link { get; set; }
+
+        //[DataType(DataType.Date)]
+        //public DateTime SentDate { get; set; }
     }
 }
