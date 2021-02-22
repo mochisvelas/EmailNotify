5cff317f2443c13928b1cb4475994291e2e25dcb
mochisvelas <velasquezmochis@gmail.com>
2021-02-22 01:04:05 -0600

    scaffolded receivers

--- Models/Receiver.cs
+++ Models/Receiver.cs
@@ -7,6 +7,8 @@ namespace EmailNotify.Models
 {
     public class Receiver
     {
+        public int Id { get; set; }
+
         public string Email { get; set; }
 
         public string Name { get; set; }
