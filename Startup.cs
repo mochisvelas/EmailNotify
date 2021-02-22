6f45efc0126d8980c7f495bb9d01b68afa7f007d
mochisvelas <velasquezmochis@gmail.com>
2021-02-22 00:51:02 -0600

    add dependencies for sql

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmailNotify.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmailNotify
 {
@@ -24,6 +26,9 @@ namespace EmailNotify
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ReceiverContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("ReceiverContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
