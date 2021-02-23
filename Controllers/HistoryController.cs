a201c76d58fad3e209e541af7969d99d285916fb
mochisvelas <velasquezmochis@gmail.com>
2021-02-23 02:04:43 -0600

    create history

---
+++ Controllers/HistoryController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmailNotify.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmailNotify.Controllers
+{
+    public class HistoryController : Controller
+    {
+        private readonly ReceiverContext _context;
+
+        public HistoryController(ReceiverContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Notification.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var notification = await _context.Notification
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (notification == null)
+            {
+                return NotFound();
+            }
+
+            return View(notification);
+        }
+    }
+}
